2544a3d99ebb6d4d6f482c73ba38f01c97880f22
MarkMartinez199504 <ruinslasher2@gmail.com>
2026-02-22 03:33:05 -0500

    installed dependancies/venv

---
+++ CLICasino/src/constants.py
@@ -0,0 +1,10 @@
+OPEN_GREETING = """
+Hello, and welcome to the CLI Casino!\n
+We have a small selection of games for you to play.\n
+Here is your complimentary chips for use here in the casino!\n
+"""
+
+DECK_SIZE = 52
+STARTING_CHIPS = 500
+SUITS = ["Clubs", "Spades", "Diamonds", "Hearts"]
+RANKS = ["A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"]
